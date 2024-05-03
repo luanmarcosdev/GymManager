d2c969d32fe98283c1fa96cd719a32d7cd463034
Luan <luanmarcosdev@gmail.com>
2024-05-02 21:15:51 -0300

    create CustomColor

---
+++ GymManager/GymManager/Utils/CustomColor.swift
@@ -0,0 +1,21 @@
+//
+//  CustomColor.swift
+//  GymManager
+//
+//  Created by Luan Arruda on 02/05/24.
+//
+
+import UIKit
+import Foundation
+
+struct CustomColor {
+    static let green = UIColor(red: 208, green: 253, blue: 62, alpha: 1)
+    static let blue = UIColor(red: 94, green: 152, blue: 166, alpha: 1)
+    static let white = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+    static let red = UIColor(red: 255, green: 36, blue: 36, alpha: 1)
+    static let black = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+    static let gray = UIColor(red: 169, green: 169, blue: 169, alpha: 1)
+    static let graySecondary = UIColor(red: 181, green: 181, blue: 181, alpha: 1)
+    static let darkMain = UIColor(red: 28, green: 28, blue: 30, alpha: 1)
+    static let darkSecondary = UIColor(red: 58, green: 58, blue: 60, alpha: 1)
+}
