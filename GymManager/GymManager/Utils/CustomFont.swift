8f00a19889e62ed8044ff40cc0a7574ecfa4c828
Luan <luanmarcosdev@gmail.com>
2024-05-06 18:43:15 -0300

    adding assets and Utils

---
+++ GymManager/GymManager/Utils/CustomFont.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFont.swift
+//  GymManager
+//
+//  Created by Luan Arruda on 06/05/24.
+//
+
+import Foundation
+
+struct CustomFont {
+    static let robotExtraLight = "RobotoSlab-ExtraLight"
+    static let robotRegular = "RobotoSlab-Regular"
+    static let robotSemiBold = "RobotoSlab-SemiBold"
+    static let robotBold = "RobotoSlab-Bold"
+    static let robotExtraBold = "RobotoSlab-ExtraBold"
+    static let robotBlack = "RobotoSlab-Black"
+}
