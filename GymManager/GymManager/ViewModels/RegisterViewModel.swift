ddbf57822b4c011df808c452ddfd4cc91d5bec92
Luan <luanmarcosdev@gmail.com>
2024-05-15 10:21:13 -0300

    organizing project

---
+++ GymManager/GymManager/ViewModels/RegisterViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterViewModel.swift
+//  GymManager
+//
+//  Created by Luan Arruda on 15/05/24.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewModel {
+    
+    // RegisterIndex - to do func validar campos de tf Name, Email e Password / func enviar dados validados para o Builder (setMainInfos)
+    
+    // RegisterGender - to do func validar se selecionou algum gender button / func enviar botao clicado para o Builder (setGender)
+    
+    // RegisterAge - to do func validar se age é valida / func enviar age selecionada para o Builder (setAge)
+    
+    // RegisterHeight -  to do func validar se height é valida / func enviar height selecionada para o Builder (setHeight)
+    
+    // RegisterWeight -  to do func validar se weight é valida / func enviar weight selecionada para o Builder (setWeight)
+    
+    // RegisterGoal -  to do func validar se goal é valida / func enviar goal selecionada para o Builder (setGoal)
+    
+    // TO DO criar um user passando as informaçoes coletadas para o UserBuilder e chamando o metodo build() e dps reset() após isso usar chamar outra classe a ser definida para salvar o usuário na API do firebase
+    
+    
+}
